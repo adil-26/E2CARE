81c61b9140f4755616a0e9c8477eef124e7e718b
I Adil-Inflow <i.adil@inflowtechnologies.com>
2026-04-25 20:38:00 +0530

    chore: add capacitor for mobile app support

---
+++ android/app/src/main/java/com/e2care/healthcompass/MainActivity.java
@@ -0,0 +1,5 @@
+package com.e2care.healthcompass;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.e2care.healthcompass',
+  appName: 'E2Care',
+  webDir: 'dist'
+};
+
+export default config;
